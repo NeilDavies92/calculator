9952eb04c64dc321d662ee4a8ec0c3eb1921d282
neildavies1 <neildavies@intelligencefusion.co.uk>
2022-08-18 10:11:15 +0100

    ask user to enter numbers and opperator

--- calculator.go
+++ calculator.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+
+	var num1, num2 float64
+	var opperator string
+
+	fmt.Print("Please Enter a number: ")
+	fmt.Scanln(&num1)
+
+	fmt.Print("Please Enter a second number: ")
+	fmt.Scanln(&num2)
+
+	fmt.Print("Please enter an opperator: ")
+	fmt.Scanln(&opperator)
 
 }
