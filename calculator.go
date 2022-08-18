fe5b3b26bf2e36c7ef12a02c55ba5482fc5a5ea0
neildavies1 <neildavies@intelligencefusion.co.uk>
2022-08-18 10:50:14 +0100

    tidy up text

--- calculator.go
+++ calculator.go
@@ -9,10 +9,10 @@ func main() {
 	var num1, num2 float64
 	var operator string
 
-	fmt.Print("Please Enter a number: ")
+	fmt.Print("Please enter a number: ")
 	fmt.Scanln(&num1)
 
-	fmt.Print("Please Enter a second number: ")
+	fmt.Print("Please enter a second number: ")
 	fmt.Scanln(&num2)
 
 	fmt.Print("Please enter an operator: ")
